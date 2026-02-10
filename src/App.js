66a83bf2ee685336beb994e4fb7842dfaf9c005b
bellwie <pedrodev503@gmail.com>
2026-02-10 20:06:38 -0300

    array com os numeros vencedores

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ function App() {
     nextSquare[i] = "x";
     setSquares(nextSquare);
   }
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  }
   return (
     <>
       <div className="board-row">
